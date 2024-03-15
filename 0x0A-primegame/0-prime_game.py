64839df3841f6816c1925168054729809d681131
clairima <mertahrima@gmail.com>
2024-03-15 05:35:30 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""Define isWineer function, solution to the Prime Game problem"""
+
+
 def is_prime(num):
+    """
+    Check if a number is prime.
+
+    Parameters:
+    - num (int): The number to be checked for primality.
+
+    Returns:
+    - bool: True if the number is prime, False otherwise.
+    """
     if num < 2:
         return False
     for i in range(2, int(num ** 0.5) + 1):
@@ -7,7 +19,20 @@ def is_prime(num):
             return False
     return True
 
+
 def isWinner(x, nums):
+    """
+    Determine the winner of each round of the prime game.
+
+    Parameters:
+    - x (int): The number of rounds.
+    - nums (list of int): An array of n for each round,
+      where n is the upper limit of the consecutive integers.
+
+    Returns:
+    - str or None: The name of the player with the most wins.
+      If the winner cannot be determined, returns None.
+    """
     wins_maria = 0
     wins_ben = 0
 
